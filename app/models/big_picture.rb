4fbd74811b2edab93cc4245320f18b8b14264b81
David T <dtran745@gmail.com>
2017-10-28 19:05:02 -0400

    Initialized objects, jobs done by rails overlords

--- app/models/big_picture.rb
+++ app/models/big_picture.rb
@@ -1,2 +1,14 @@
 class BigPicture < Picture
+
+  attr_accessor :location
+  attr_accessor :time
+  attr_accessor :culture
+  attr_accessor :links
+
+  def initialize
+    @location = 'The Forgotten Realms'
+    @time = '-7800 DR'
+    @culture = 'Predominantly dominated by Djinn'
+    @links = '{}'
+  end
 end
--- app/models/little_picture.rb
+++ app/models/little_picture.rb
@@ -1,2 +1,7 @@
 class LittlePicture < Picture
+  attr_accessor :tags
+
+  def initialize
+    @tags = 'fun chaotic-evil steampunk'
+  end
 end
