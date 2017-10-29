e83aa456f215f5a9dd25ce5d41c9e5e4445ed67b
Jerome Andaya <jandaya@bu.edu>
2017-10-28 21:53:05 -0400

    App initialization

---
+++ app/helpers/big_picture_helper.rb
@@ -0,0 +1,2 @@
+module BigPictureHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'app/index'
+
+  get 'big_picture/read'
+
+  get 'big_picture/update'
+
+  get 'big_picture/delete'
+
+  get 'big_picture/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
