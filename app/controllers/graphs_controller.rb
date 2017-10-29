2ad88d709dbc73e0c2ce7e554478c04f9b2fe896
Julie Ruiz <julie.anne.ruiz@gmail.com>
2017-10-29 11:21:18 -0400

    I added breadcrumbs and toolkit on graph page. Then I fixed the footer so it should be on the bottom right side of the whole site.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,6 +1,15 @@
 class GraphsController < ApplicationController
 	def graphs
 		@title = 'Adventure';
-		@content = 'Mapping out the journey';
 	end
+
+<div>
+<nav class="breadcrumb">
+  <a class="breadcrumb-item" href="#">Home</a>
+  <a class="breadcrumb-item" href="#">Library</a>
+  <a class="breadcrumb-item" href="#">Data</a>
+  <span class="breadcrumb-item active">Bootstrap</span>
+</nav>
+</div>
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028212448) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20171029003023) do
 
   create_table "big_pictures", force: :cascade do |t|
     t.string "location"
@@ -42,11 +35,17 @@ ActiveRecord::Schema.define(version: 20171028212448) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.text "password"
-
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
