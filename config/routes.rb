e7f58cbac35b9c30c5f9cbf34becdeb47b80c20d
Jerome Andaya <jandaya@bu.edu>
2017-11-06 11:13:57 -0500

    rebase to master

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :big_picture
+<<<<<<< HEAD
   # get     #index - list of all big pictures
   # get     #new - form for create object
   # post    #create - actual request
@@ -13,6 +14,10 @@ Rails.application.routes.draw do
 
   get 'graph/index'
 
+=======
+    get '/big_picture/new', to: 'big_picture#create'
+
+>>>>>>> preliminary controller, create not routed
   root 'app#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
